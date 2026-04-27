5b1cd5ffe14f802d6d5b11417a66f71ba8018300
brightlane <benitopalmarini@aol.com>
2026-04-27 06:30:58 -0400

    Refactor sitemap generation for improved readability

--- sitemap_gen.py
+++ sitemap_gen.py
@@ -5,19 +5,14 @@ DOMAIN = "https://brightlane.github.io/Many-Chat"
 
 def generate_sitemap():
     pages = os.listdir('industries')
-    sitemap_xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
-    sitemap_xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+    sitemap_xml = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
 
     for page in pages:
         if page.endswith('.html'):
             url = f"{DOMAIN}/industries/{page}"
-            lastmod = datetime.now().strftime('%Y-%m-%d')
-            sitemap_xml += f"  <url>\n    <loc>{url}</loc>\n    <lastmod>{lastmod}</lastmod>\n    <changefreq>daily</changefreq>\n  </url>\n"
+            sitemap_xml += f"  <url><loc>{url}</loc><lastmod>{datetime.now().strftime('%Y-%m-%d')}</lastmod></url>\n"
 
     sitemap_xml += '</urlset>'
-    
-    with open('sitemap.xml', 'w') as f:
-        f.write(sitemap_xml)
-    print("Vulture Sitemap Updated: sitemap.xml")
+    with open('sitemap.xml', 'w') as f: f.write(sitemap_xml)
 
 generate_sitemap()
